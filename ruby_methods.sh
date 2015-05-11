7b4efc91e8eb8179593d76fefaca29e242bef1fb
Ethan Hathaway <erhathaway@gmail.com>
2015-05-11 17:45:21 -0400

    update use instructions to ?rb

--- ruby_methods.sh
+++ ruby_methods.sh
@@ -6,7 +6,7 @@
 #		$ vim ~/.zshrc
 #		alias \?rb='/home/ethan/Dropbox/shell/ruby_methods.sh'
 #	3) reload .zshrc: $ source ~/.zshrc
-#	4) to use: $ ? '{}.inject'
+#	4) to use: $ ?rb '{}.inject'
 
 # ruby_method - A script to produce documentation for a ruby method
 echo "show-doc $1" | pry
