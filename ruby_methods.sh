a9f08bd8bb7dfe71228a467440f6cf215b3826c4
Ethan Hathaway <erhathaway@gmail.com>
2015-05-11 17:42:04 -0400

    add shell script to query ruby method documentation

---
+++ ruby_methods.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# to use:
+#       1) change file permissions: $ chmod 755 ruby_methods.sh
+#       2) add alias to .zshrc: 
+#		$ vim ~/.zshrc
+#		alias \?rb='/home/ethan/Dropbox/shell/ruby_methods.sh'
+#	3) reload .zshrc: $ source ~/.zshrc
+#	4) to use: $ ? '{}.inject'
+
+# ruby_method - A script to produce documentation for a ruby method
+echo "show-doc $1" | pry
+
+
+
+
